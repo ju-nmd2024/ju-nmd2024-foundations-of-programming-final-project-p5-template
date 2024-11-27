726231c9b83dfd0425deb81ac32cc673b1f2c0ec
DominikRyd <62755466+DominikRyd@users.noreply.github.com>
2024-11-27 09:08:07 +0100

    update 0.1
    Har lagt in functions för röda och gröna hinder

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 function startScreen(){
   background(255,255,255);
   text("start",160,50);
+
+  
+  obstacleGreen(100,100);
 }
 
 function gameScreen(){
@@ -11,6 +14,18 @@ function resultScreenLoss(){
   background(255,255,255);
 }
 
+function obstacleGreen(x,y){
+  push ();
+  fill (34,177,76);
+  rect (0,0,70,10,10);
+  pop ();
+}
+function obstacleRed(x,y){
+  push ();
+  fill (237,28,36);
+  rect (0,0,70,10,10);
+  pop ();
+}
 function setup (){
   createCanvas(400,560);
   
@@ -18,3 +33,11 @@ function setup (){
 
 //Änsålänge kan vi använda denna för att ändra till vilken skärm vi vill ha för att jobba med en specifik skärm
 let state = "start"; 
+
+function draw(){
+  if (state === "start") {
+      startScreen();
+  } else if (state === "game") { 
+      gameScreen();
+  }
+}
