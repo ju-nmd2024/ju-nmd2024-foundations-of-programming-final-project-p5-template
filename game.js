f701f2b8e9dd388e4f84e4266a40f1bad314117b
Aura <auraloco@gmail.com>
2024-11-27 16:27:06 +0100

    Update 5
    Made a prototype for the character and snowballs. Made the character move and you can now choose how to move the character, WASD, arrow keys or mouse.

--- game.js
+++ game.js
@@ -1,9 +1,7 @@
+//Class imports
 import MountainBackground from "./mountainBackground.js";
 
-const mountainB = new MountainBackground(100, 330);
-
-state = "start";
-
+//Setup function with canvas size
 function setup() {
   createCanvas(900, 900);
 
@@ -14,7 +12,61 @@ function setup() {
 }
 window.setup = setup;
 
+//Class variables
+const mountainB = new MountainBackground(100, 330);
+
+//State variable
+state = "start";
+
+//Control for character variable
+let controlMode = "arrow";
+
+//Character variable
+let characterX = 450;
+
+//Function for when the mouse is pressed
 function mousePressed() {
+  //If statement for when the mouse clicks the buttons in the options screen
+  //To be able to change controls for the character movement
+  if (state === "options") {
+    if (
+      mouseX > width / 2 - 335 &&
+      mouseX < width / 2 - 85 &&
+      mouseY > height / 2 + 50 &&
+      mouseY < height / 2 + 130
+    ) {
+      controlMode = "WASD";
+      state = "start";
+    }
+    if (
+      mouseX > width / 2 + 75 &&
+      mouseX < width / 2 + 325 &&
+      mouseY > height / 2 + 50 &&
+      mouseY < height / 2 + 130
+    ) {
+      controlMode = "arrow";
+      state = "start";
+    }
+    if (
+      mouseX > width / 2 - 90 &&
+      mouseX < width / 2 + 110 &&
+      mouseY > height / 2 + 200 &&
+      mouseY < height / 2 + 280
+    ) {
+      controlMode = "mouse";
+      state = "start";
+    }
+    if (
+      mouseX > width / 2 - 425 &&
+      mouseX < width / 2 - 95 &&
+      mouseY > height / 2 + 350 &&
+      mouseY < height / 2 + 430
+    ) {
+      state = "start";
+    }
+  }
+
+  //If statement for when the buttons clicks on the start screen
   if (state === "start") {
     if (mouseX > 200 && mouseX < 400 && mouseY > 650 && mouseY < 730) {
       state = "game";
@@ -24,6 +76,7 @@ function mousePressed() {
     }
   }
 
+  //If statements for when the mouse clicks the buttons in the end screen
   if (state === "result") {
     if (mouseX > 320 && mouseX < 620 && mouseY > 600 && mouseY < 680) {
       state = "start";
@@ -33,7 +86,27 @@ function mousePressed() {
     }
   }
 }
+window.mousePressed = mousePressed;
 
+function snowBalls() {
+  push();
+  stroke(0, 0, 0);
+  fill(255, 255, 255);
+  ellipse(width / 4 - 100, height / 2 - 50, 125);
+  ellipse(width / 2, height / 2 - 50, 125);
+  ellipse(width - 125, height / 2 - 50, 125);
+  pop();
+}
+
+function character() {
+  push();
+  stroke(0, 0, 0);
+  fill(255, 255, 255);
+  rect(characterX - 50, height - 150, 100, 100);
+  pop();
+}
+
+//Function for the start screen, with buttons
 function startScreen() {
   background(133, 206, 244);
   mountainB.draw();
@@ -55,22 +128,26 @@ function startScreen() {
   pop();
 }
 
+//Fucntion for the options screen with buttons
 function optionScreen() {
   background(133, 206, 244);
   mountainB.draw();
 
+  //Buttons rectangles
   fill(255, 244, 220);
   rect(width / 2 - 335, height / 2 + 50, 250, 80, 30);
   rect(width / 2 + 75, height / 2 + 50, 250, 80, 30);
   rect(width / 2 - 90, height / 2 + 200, 200, 80, 30);
   rect(width / 2 - 425, height / 2 + 350, 330, 80, 30);
 
+  //Text outside buttons
   push();
   fill(0, 0, 0);
   textSize(50);
   textStyle(BOLD);
   text("Choose between:", width / 2 - 175, height / 2 - 50);
 
+  //Text in buttons
   textSize(30);
   text("WASD KEYS", width / 2 - 300, height / 2 + 100);
   text("ARROW KEYS", width / 2 + 100, height / 2 + 100);
@@ -79,31 +156,63 @@ function optionScreen() {
   pop();
 }
 
+//Function for the game screen
 function gameScreen() {
   background(133, 206, 244);
   mountainB.draw();
+
+  //Character movement
+  if (controlMode === "arrow") {
+    if (keyIsDown(37)) {
+      characterX -= 15;
+    }
+    if (keyIsDown(39)) {
+      characterX += 15;
+    }
+  } else if (controlMode === "WASD") {
+    if (keyIsDown(65)) {
+      characterX -= 15;
+    }
+    if (keyIsDown(68)) {
+      characterX += 15;
+    }
+  } else if (controlMode === "mouse") {
+    characterX = mouseX;
+  }
+
+  //Constrains the character from moving outside the canvas
+  characterX = constrain(characterX, 50, width - 50);
+
+  snowBalls();
+  character();
+  //character.draw();
 }
 
+//Function for the result screen
 function resultScreen() {
   background(133, 206, 244);
   mountainB.draw();
 
+  //Rectangles for button for restart and start screen
   fill(255, 244, 220);
   rect(width / 2 - 130, height / 2 + 150, 300, 80, 30);
   rect(width / 2 - 130, height / 2 + 300, 300, 80, 30);
 
+  //Game over text
   push();
   fill(0, 0, 0);
   textSize(70);
   textStyle(BOLD);
   text("GAME OVER", width / 2 - 200, height / 2 - 20);
 
+  //Text in buttons
   textSize(30);
   text("START SCREEN", width / 2 - 95, height / 2 + 200);
   text("RESTART GAME", width / 2 - 100, height / 2 + 350);
   pop();
 }
 
+//Draw function using states
 function draw() {
   if (state === "start") {
     startScreen();
@@ -115,3 +224,4 @@ function draw() {
     resultScreen();
   }
 }
+window.draw = draw;
