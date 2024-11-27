7a27f4575b955644e4dce5a1789c1b959d66335f
DominikRyd <62755466+DominikRyd@users.noreply.github.com>
2024-11-27 08:43:42 +0100

    Screens
    har lagt till olika startskärmar

--- game.js
+++ game.js
@@ -1,10 +1,20 @@
-function setup() {
-  createCanvas(800, 600);
+function startScreen(){
+  background(255,255,255);
+  text("start",160,50);
 }
 
-function draw() {
-  background(255, 140, 0);
+function gameScreen(){
+  background(255,255,255);
 }
-//hej dominik
 
-// test
+function resultScreenLoss(){
+  background(255,255,255);
+}
+
+function setup (){
+  createCanvas(400,560);
+  
+}
+
+//Änsålänge kan vi använda denna för att ändra till vilken skärm vi vill ha för att jobba med en specifik skärm
+let state = "start"; 
